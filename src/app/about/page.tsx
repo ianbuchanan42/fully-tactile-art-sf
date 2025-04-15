ef79b753ab25e1ce35423c1ed488e2c21f03bdf5
Ian Buchanan <get.ian.buchanan@gmail.com>
2025-04-15 08:31:52 -0700

    fixe Jerry title'

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -117,7 +117,7 @@ export default function AboutPage() {
             <div className='bg-white p-6 rounded-lg shadow-md'>
               <h3 className='text-xl font-semibold mb-2'>Jerry Kuns</h3>
               <p className='text-gray-700 mb-3'>
-                Program Director & Accessibility Specialist
+                Co-founder & Accessibility Consultant
               </p>
               <p>
                 Jerry is an artist, social worker, and educator who has been
